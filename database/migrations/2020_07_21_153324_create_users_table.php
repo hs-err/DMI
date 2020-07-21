c572f62e8604a8540ae7ba08f4905a374365f217
hs-err <1102280066@qq.com>
2020-07-22 00:38:21 +0800

    添加:迁移文件

--- database/migrations/2020_07_21_153324_create_users_table.php
+++ database/migrations/2020_07_21_153324_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateGeneratesTable extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreateGeneratesTable extends Migration
      */
     public function up()
     {
-        Schema::create('generates', function (Blueprint $table) {
-            $table->uuid('id')->unique();
-            $table->integer('user_id');
-            $table->longText('data');
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -28,6 +29,6 @@ class CreateGeneratesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('generates');
+        Schema::dropIfExists('users');
     }
 }
