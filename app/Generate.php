409de91f69277a461d97ea0a7d5d42d6ec00cf30
hs-err <1102280066@qq.com>
2020-07-21 20:47:30 +0800

    添加:Generate模型

---
+++ app/Generate.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Generate
+ * @package App
+ * @property int user_id
+ * @property string data
+ */
+class Generate extends Model
+{
+    //
+}
