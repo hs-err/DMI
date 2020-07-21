844eb670d384010d63ccaf6a64a9ed0716bcd0e3
hs-err <1102280066@qq.com>
2020-07-22 00:41:17 +0800

    添加:添加模型控制类路由 修改:ide-helper

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,8 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+/** @var \Laravel\Lumen\Routing\Router $router */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+$router->get('/{uuid:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}', 'DrawController@draw');
+
+$router->post('/auth/login', 'UserController@login');
