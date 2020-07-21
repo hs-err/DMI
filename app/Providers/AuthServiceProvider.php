399444882b3b4eb897b9cb50c7afca44ca836529
hs-err <1102280066@qq.com>
2020-07-22 00:47:07 +0800

    修改:token键名

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use App\User;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -31,9 +30,10 @@ class AuthServiceProvider extends ServiceProvider
         // the User instance via an API token or any other method necessary.
 
         $this->app['auth']->viaRequest('api', function ($request) {
-            if ($request->input('api_token')) {
-                return User::where('api_token', $request->input('api_token'))->first();
+            if ($request->input('remember_token')) {
+                return User::where('remember_token', $request->input('remember_token'))->first();
             }
+            return null;
         });
     }
 }
